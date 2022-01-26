e813b63b87cf224c9108ca6c804559530967a1ec
RedLime <redlimerl@gmail.com>
2022-01-26 13:10:01 +0900

    Migrate 1.18

--- src/main/java/com/redlimerl/detailab/DetailArmorBar.java
+++ src/main/java/com/redlimerl/detailab/DetailArmorBar.java
@@ -18,7 +18,7 @@ import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.loading.FMLEnvironment;
-import net.minecraftforge.fmlclient.ConfigGuiHandler;
+import net.minecraftforge.client.ConfigGuiHandler;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
